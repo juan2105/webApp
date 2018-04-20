d825fd387f6860ccaec40fbabb0e78d61924a10c
juan2105 <juandguez21@gmail.com>
2018-04-20 14:34:52 -0500

    Ejercicios

--- doscaminos/src/app/app.component.ts
+++ doscaminos/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Estado } from './estado';
 })
 export class AppComponent {
   title = 'app works!';
+  nombre = "jose";
   carreras = ["mantto","tic","tu","ga","te"];
   estados = [
     new Estado(1,"Aguascalientes","AGS"),
@@ -15,4 +16,26 @@ export class AppComponent {
     new Estado(3,"Chiapas","CHP"),
     new Estado(4, "Durango", "DRG")
   ];
+ 
+  visible = false;
+  nom0=false;
+  nom1=false;
+  nom2=false;
+  nom3=false;
+  
+  name: string  = "";
+  leyenda:string = "";
+
+
+  /**Declarar una función */
+  mostrar(){
+    if(!this.visible){
+      this.visible = true;
+      this.leyenda = "Activar";
+    }else{
+
+      this.visible = false;
+      this.leyenda = "Desactivar";
+    }
+  }
 }
